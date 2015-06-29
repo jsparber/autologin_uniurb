52c5ffe764de70b9c903d3ccbc2592e3edc60155
Julian Sparber <julian@sparber.net>
2015-06-29 15:06:44 +0200

    [fix+feat] replaced iwconfig with iw and add working support for sad and UNIURB wifis

--- autologin.sh
+++ autologin.sh
@@ -25,87 +25,88 @@
 
 first_run()
 {
-        echo "Type Username"
-        read username
-        echo "Type Password"
-        read password
-        echo "Type realm"
-        read realm
-        
-        echo "username=$username" > $DIR/config
-        echo "password=$password" >> $DIR/config
-        echo "realm=$realm" >> $DIR/config
+  echo "Type Username"
+  read username
+  echo "Type Password"
+  read password
+  echo "Type realm"
+  read realm
+
+  echo "username=$username" > $DIR/config
+  echo "password=$password" >> $DIR/config
+  echo "realm=$realm" >> $DIR/config
 }
 get_gateway()
 {
-	GATEWAY=`ip addr show | grep "inet " | grep -v 127.0.0.1 | head -n 1 | cut --delimiter=" " -f 6 | cut --delimiter=. -f 1-3`.1
+  GATEWAY=`ip addr show | grep "inet " | grep -v 127.0.0.1 | head -n 1 | cut --delimiter=" " -f 6 | cut --delimiter=. -f 1-3`.1
 }
 
 load_user_data()
 {
-	# Verify if there is a config file, if not create that
-	if [ "`ls $DIR/config 2> /dev/zero`" != "$DIR/config" ] ; then
-		first_run
-	fi
-	path=$DIR/config
-	username=`cat $path | grep username | cut -c 10-`
-	realm=`cat $path | grep realm | cut -c 7-`
-	password=`cat $path | grep password | cut -c 10-`
+  # Verify if there is a config file, if not create that
+  if [ "`ls $DIR/config 2> /dev/zero`" != "$DIR/config" ] ; then
+    first_run
+  fi
+  path=$DIR/config
+  username=`cat $path | grep username | cut -c 10-`
+  realm=`cat $path | grep realm | cut -c 7-`
+  password=`cat $path | grep password | cut -c 10-`
 }
 
 check_connection()
 {
-	echo Check connection ...
-	rm prelogin
-	curl -L -s http://$GATEWAY:3990/prelogin > prelogin
-	checkInternet=`cat prelogin | grep logoff`
-	if [ "$checkInternet" != "" ] ; then
-		echo Internet Works
-	else
-		echo Internet not Works
-	fi
+  echo Check connection ...
+  rm prelogin
+  curl -L -s http://$GATEWAY:3990/prelogin > prelogin
+  checkInternet=`cat prelogin | grep logoff`
+  if [ "$checkInternet" != "" ] ; then
+    echo Internet Works
+  else
+    echo Internet not Works
+  fi
 }
 
 logon()
 {
-	echo Doing login ... 
-	#Download Lgoin website
-	chal=`curl -L -s http://$GATEWAY:3990/prelogin | grep chal -a | cut -c 51-82`
+  echo Doing login ... 
+  #Download Lgoin website
+  chal=`curl -L -s http://$GATEWAY:3990/prelogin | grep chal -a | cut -c 51-82`
   echo $GATEWAY
 
-	url="https://radius.uniurb.it/URB/test.php?chal="$chal"&uamip="$GATEWAY"&uamport=3990&userurl=&UserName="$username"&Realm="$realm"&Password="$password"&form_id=69889&login=login"
-	password=`curl -L -s $url | grep -a password`
-        # calculate start and end of hashed password
-	end=`expr $(echo $password | wc -m) - 3`
-	start=`expr $end - 31`
-        # extract the password
-	password=`echo $password | cut -c $start-$end`
-	url="http://"$GATEWAY":3990/logon?username="$username"@"$realm"&password="$password
-	res=`curl -L -s $url`
-	echo Done
+  url="https://radius.uniurb.it/URB/test.php?chal="$chal"&uamip="$GATEWAY"&uamport=3990&userurl=&UserName="$username"&Realm="$realm"&Password="$password"&form_id=69889&login=login"
+  password=`curl -L -s $url | grep -a password`
+  # calculate start and end of hashed password
+  end=`expr $(echo $password | wc -m) - 3`
+  start=`expr $end - 31`
+  # extract the password
+  password=`echo $password | cut -c $start-$end`
+  url="http://"$GATEWAY":3990/logon?username="$username"@"$realm"&password="$password
+  res=`curl -L -s $url`
+  echo Done
 }
 logon_uwic()
 {
-	curl --data "v=pda&username=$username&password=$password&rad=%40$realm&expire=999" https://gateway.wireless-campus.it/logincheck.php
+  curl --data "v=pda&username=$username&password=$password&rad=%40$realm&expire=999" https://gateway.wireless-campus.it/logincheck.php
 }
 logon_sad()
 {
-	echo connection to Sad Wifi
-  curl 'https://sadwifi.uniurb.it:8001/' -H 'Host: sadwifi.uniurb.it:8001' -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:33.0) Gecko/20100101 Firefox/33.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8' -H 'Accept-Language: en,it;q=0.8,de;q=0.5,en-us;q=0.3' -H 'Accept-Encoding: gzip, deflate' -H 'DNT: 1' -H 'Referer: https://sadwifi.uniurb.it:8001/' -H 'Connection: keep-alive' --data "user=$username&auth_pass=$password&Realm=$realm&auth_user=$username%40$realm&redirurl=%2F&accept=GO"
-
+  res=`curl --data "user=$username&auth_pass=$password&Realm=$realm&auth_user=$username%40$realm&redirurl=&accept=GO" https://sadwifi.uniurb.it:8003 -s`
+  logout_id=`echo ${res} | cut -d " " -f 60 | cut -d '"' -f 2`
+  #curl --data "logout_id=$logout_id&zone=sadwifi&logout=Logout" 'https://sadwifi.uniurb.it:8003/' 
+  echo For logoff run:
+  echo "curl --data 'logout_id=$logout_id&zone=sadwifi&logout=Logout' 'https://sadwifi.uniurb.it:8003/'"
 }
 logoff()
 {
-	echo Doing logoff ...
-	curl -L http://$GATEWAY:3990/logoff
-	echo Done
+  echo Doing logoff ...
+  curl -L http://$GATEWAY:3990/logoff
+  echo Done
 }
 get_ap()
 {
-	if [ "`iwconfig 2> /dev/zero | grep Not-Associated`" == "" ] ; then
-		WIFINETWORK=`iwconfig 2> /dev/zero | cut -f 2 -d '"' | head -1`
-	fi
-	echo Wifinetwork = $WIFINETWORK
+  interface=`iw dev | grep Interface | cut -d " " -f 2` 
+  WIFINETWORK=`iw dev $interface link | grep SSID | cut -d " " -f 2`
+  echo Wifinetwork = $WIFINETWORK
 }
 # Set Default Settings
 WIFINETWORK=STILABWIFI
@@ -118,24 +119,24 @@ get_gateway
 get_ap
 load_user_data
 if [ "$WIFINETWORK" == "UWiC" ] ; then
-	echo connect to UWiC
-	logon_uwic
+  echo connect to UWiC
+  logon_uwic
 else
-	if [ "$WIFINETWORK" == "SAD-UNIURB" ] ; then
-		echo connect to SAD-UNIURB
-		logon_sad
-	else
-		#Start Connecting process
-		logon
-		#check_connection
-	fi
+  if [ "$WIFINETWORK" == "SAD-UNIURB" ] ; then
+    echo connect to SAD-UNIURB
+    logon_sad
+  else
+    if [ "$WIFINETWORK" == "UNIURB" ] ; then
+      echo connect to UNIURB
+      logon_sad
+    else
+      #Start Connecting process
+      logon
+      #check_connection
+    fi
+  fi
 fi
 #logoff
 #check_connection
 
-date
-echo wait 40 min and relogin
-sleep 2400
-date
-exec $DIR/autologin.sh
 exit 0
